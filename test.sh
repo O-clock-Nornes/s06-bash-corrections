d37af160fa0a4f582fe6605db3540933e25c9570
Aurelien <aurelien.bras@gmail.com>
2024-11-25 12:05:33 +0100

    correction e03 soir

--- test.sh
+++ test.sh
@@ -1,32 +1,10 @@
 #!/bin/bash
-
-OPTSTRING=":abc"
-
-while getopts ${OPTSTRING} opt; do
-	case ${opt} in
-		a)
-			echo "Options -a faite"
-			;;
-		b)
-			echo "Options -b faite"
-			;;
-		c)
-			echo "Options -c faite"
-			;;
-		?)
-			echo "Invalid option - ${OPTARG}"
-			exit 1
-			;;
-	esac
-done
-
-
-echo "Paramètre numéro 1 $1"
-echo "Paramètre numéro 2 $2"
-echo "Paramètre numéro 3 $3"
-echo "Paramètre numéro 4 $4"
-echo "Paramètre numéro 5 $5"
-
-if [ "$1" == "un" ] ; then
-	echo "Le premier paramètre est bien un, bravo"
+if [ ! -d ~/analyses ] ; then
+	mkdir ~/analyses
 fi
+
+cat /etc/passwd | awk -F ":" '{if($3>=1000) print $1}' | grep -v nobody |
+while read username ; do
+	echo "Utilisateur: $username"
+	groups $username >> ~/analyses/user_groups.log
+done
