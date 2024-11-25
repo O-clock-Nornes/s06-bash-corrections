5bf9ca0e4a91c2c22fa3b005237b6fe8f06243f5
Aurelien <aurelien.bras@gmail.com>
2024-11-25 10:26:07 +0100

    traiter des options avec getopts

--- test.sh
+++ test.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-ls -1 *.txt |
-while read fichier; do
-	echo "Lecture de $fichier"
+OPTSTRING=":abc"
+
+while getopts ${OPTSTRING} opt; do
+	case ${opt} in
+		a)
+			echo "Options -a faite"
+			;;
+		b)
+			echo "Options -b faite"
+			;;
+		c)
+			echo "Options -c faite"
+			;;
+		?)
+			echo "Invalid option - ${OPTARG}"
+			exit 1
+			;;
+	esac
 done
 
-if [ ! -d backup ] ; then
-	mkdir backup
-fi
-if [ -d backup ] ; then
-	cp *.txt backup/
+
+echo "Paramètre numéro 1 $1"
+echo "Paramètre numéro 2 $2"
+echo "Paramètre numéro 3 $3"
+echo "Paramètre numéro 4 $4"
+echo "Paramètre numéro 5 $5"
+
+if [ "$1" == "un" ] ; then
+	echo "Le premier paramètre est bien un, bravo"
 fi
